96742fae2bbd0805519d1b73c25d1b63ed428abc
ArtSoft <admin@ai24.eu>
2024-03-20 20:40:15 +0100

    Initial commit

---
+++ immich-slideshow.js
@@ -0,0 +1,165 @@
+var ImmichSlideshowVersion = "1.0.0";
+var PlaceholderSrc = "/local/immich-slideshow/placeholder.png";
+
+import {
+  LitElement,
+  html,
+  css,
+} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
+
+class ImmichSlideshow extends LitElement {
+
+  static get properties() {
+    return {
+      hass: {},
+      config: {}
+    };
+  }
+
+  render() {
+    return html`
+     <ha-card style="overflow:hidden;">
+      <div class="wrapper" style="height:${this.config.height}">
+       <img class="bottom" src="${PlaceholderSrc}" alt="immich-slideshow">
+       <img class="top hidden" @transitionend="${this._onTransitionEnd}" src="${PlaceholderSrc}" alt="immich-slideshow">
+      </div>
+     </ha-card>
+    `;
+  }
+
+  firstUpdated() {
+    this._doSlideshow();
+  }
+
+  _getImg(className) {
+    return this.renderRoot.querySelector(".wrapper img." + className);
+  }
+
+  _onTransitionEnd(e) {
+    var top = this._getImg("top");
+    var bottom = this._getImg("bottom");
+    bottom.src = top.src;
+    top.classList.replace("visible", "hidden");
+  }
+
+  _slideshow = null;
+  _doSlideshow() {
+    if (this._slideshow != null) {
+      clearTimeout(this._slideshow);
+      this._slideshow = null;
+    }
+    this._nextImage();
+    this._slideshow = setTimeout(() => {
+      this._doSlideshow();
+      this._slideshow = null;
+    }, this.config.slideshow_interval * 1000);
+  }
+
+  async _nextImage() {
+    var top_img = this._getImg("top");
+    top_img.src = await this._getNextImageURL();
+    top_img.classList.replace("hidden", "visible");
+  }
+
+  setConfig(config) {
+    if (!config.apikey)
+      throw new Error("You need to define an apikey");
+
+    if (!config.host)
+      throw new Error("You need to define an host");
+
+    //Robimy kopię obiektu, inaczej nie można dodawać do niego wlasciwosci
+    var isconfig = Object.create(config);
+
+    if (!isconfig.height)
+      isconfig.height = "100%";
+    if (!isconfig.slideshow_interval || isconfig.slideshow_interval < 6)
+      isconfig.slideshow_interval = 6;
+
+    //console.groupCollapsed("immich-slideshow setting (usunąć to jak już będzie wszystko ok)");
+    //console.log("apikey:"+isconfig.apikey);
+    //console.log("host:"+isconfig.host);
+    //console.log("height:"+isconfig.height);
+    //console.log("slideshow_interval:"+isconfig.slideshow_interval);
+    //console.log("--------------------------");
+    //console.log("PrevConfig:"+this.config);
+    //console.groupEnd();
+    this.config = isconfig;
+  }
+
+  getCardSize() {
+    return 1;
+  }
+
+  async _apiGet(url) {
+    let call_url = new URL("api/" + url, this.config.host);
+    //console.log("apiCall => "+call_url);
+    let requestOptions =
+    {
+      method: 'GET',
+      credentials: 'include',
+      headers:
+      {
+        'X-Api-Key': `${this.config.apikey}`
+      }
+    };
+    return fetch(call_url, requestOptions);
+  }
+
+  async _getRandomID() {
+    return this._apiGet("asset/random")
+      .then(response => response.json())
+      .then(json => json[0].id);
+  }
+
+  async _getNextImageURL() {
+    var id = await this._getRandomID();
+    return this._apiGet("asset/thumbnail/" + id + "?format=JPEG").
+      then(response => response.blob()).
+      then(blob => URL.createObjectURL(blob));
+  }
+
+  static get styles() {
+    return css`
+    .wrapper {
+     position: relative;
+     width: 100%;
+     overflow: hidden;
+    }
+
+    img {
+     width: 100%;
+     height: 100%;
+     object-fit: cover;
+    }
+
+    img.bottom {
+     position: relative;
+    }
+
+    img.top {
+     position: absolute;
+     top: 0;
+     left: 0;
+    }
+
+    img.visible {
+     transition: all 5s ease-in;
+     opacity: 1;
+    }
+
+    img.hidden {
+     transition: none;
+     opacity: 0;
+    }`;
+  }
+}
+
+customElements.define("immich-slideshow", ImmichSlideshow);
+
+//INFO----------------------------------------------------------------------------------------------------------------
+let infoStyles = [
+  "color: #fff",
+  "background-color: #444"
+].join(";");
+console.info("%cImmichSlideshow Version:" + ImmichSlideshowVersion, infoStyles);
